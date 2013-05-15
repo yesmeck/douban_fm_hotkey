73d67b5fa7f98531cb1e5bbc912e1facbbd5b44e
Meck <yesmeck@gmail.com>
2013-05-16 02:04:07 +0800

    载入歌曲信息

--- chrome-extension/douban_fm_hotkey.js
+++ chrome-extension/douban_fm_hotkey.js
@@ -1,26 +1,77 @@
 var faye = document.createElement('script');
 faye.src = 'http://localhost:1988/faye.js';
 faye.onload = function() {
-  exec(control);
+  exec(init);
 }
 document.documentElement.firstChild.appendChild(faye);
 
-var control = function() {
-  var client = new Faye.Client('http://localhost:1988/faye');
-  client.subscribe('/hotkey', function(message) {
-    console.log(message);
-    DBR.swf().act(message.cmd)
+var init = function() {
+  window.dfhClient = new Faye.Client('http://localhost:1988/faye');
 
-    client.publish('/state', {
+  dfhClient.subscribe('/hotkey', function(message) {
+    DBR.act(message.cmd)
+    if (message.cmd == 'pause' || message.cmd == 'love') {
+      dfhSendSongInfo({
+        song: FM.getCurrentSongInfo(),
+        radio: {
+          is_paused: DBR.is_paused(),
+          selected_like: DBR.selected_like()
+        }
+      });
+    }
+  });
+
+  dfhClient.subscribe('/get_state', function() {
+    dfhSendSongInfo({
       song: FM.getCurrentSongInfo(),
       radio: {
         is_paused: DBR.is_paused(),
         selected_like: DBR.selected_like()
       }
     });
-
   });
-}
+
+  window.dfhSendSongInfo = function(info) {
+    dfhClient.publish('/state', info);
+  };
+
+  var channelName = function(id) {
+    id = parseInt(id, 10);
+    if (id === 0 || id === -9) {
+      return "私人兆赫"
+    } else {
+      if (id === -3 || id === -8) {
+        return "红心兆赫"
+      } else {
+        return $("#fm-channel-list .channel[cid=" + id + "] .chl_name").first().text()
+      }
+    }
+  }
+
+  setTimeout(function() {
+    window.originExtStatusHandler = window.extStatusHandler;
+    window.extStatusHandler = function(info) {
+      var parsedInfo = $.parseJSON(info);
+      if (parsedInfo.type == 'start') {
+        dfhSendSongInfo({
+          song: {
+            artistName: parsedInfo.song.artist,
+            channelName: channelName(parsedInfo.channel),
+            coverUrl: parsedInfo.song.picture,
+            songName: parsedInfo.song.title
+          },
+          radio: {
+            is_paused: false,
+            selected_like: parsedInfo.song.like
+          }
+        });
+      }
+      originExtStatusHandler(info);
+    };
+  }, 2000 );
+
+};
+
 
 // http://stackoverflow.com/a/9906932/398988
 function exec(fn) {
@@ -35,6 +86,6 @@ function exec(fn) {
   script.setAttribute("type", "application/javascript");
   script.textContent = '(' + fn + ')(' + args + ');';
   document.documentElement.appendChild(script); // run the script
-  document.documentElement.removeChild(script); // clean up
+  // document.documentElement.removeChild(script); // clean up
 }
 
